a7a7bf03b45df6d8342de9115372ced038e462e9
manuelguerra1 <chingo2005@gmail.com>
2022-12-07 14:09:07 -0300

    resolvimos un conflicto

--- funciones.js
+++ funciones.js
@@ -1,13 +1,10 @@
 console.log()
 console.log()
 
-<<<<<<< HEAD
 let animales = ["perro", "gato", "oveja"]
 console.log(animales);
 let animalesPusheado = animales.pop()
 console.log(animalesPusheado);
-=======
 const saludar = nombre => console.log(`Hola ${nombre}, como estas?`)
 saludar("jhony")
 
->>>>>>> aac884872a9e900346a136c83c9f6d8005d5e04e
